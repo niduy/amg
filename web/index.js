a1a284043fe91712130b575e68b56becf15d2e0f
niduy <37757504+niduy@users.noreply.github.com>
2022-10-25 19:36:28 +0400

    update css

--- web/index.js
+++ web/index.js
@@ -9,6 +9,8 @@ const handleImageInputChange = async (event, asciiElement, imageElement) => {
 	const file = event.target.files[0];
 	const isGif = file.type.split('/').pop() === 'gif';
 
+	imageInputTextElement.innerHTML = file.name;
+
 	const imageUrl = URL.createObjectURL(file);
 
 	const file_buffer = await file.arrayBuffer();
@@ -29,6 +31,8 @@ const handleImageInputChange = async (event, asciiElement, imageElement) => {
 };
 
 const imageInputElement = document.getElementById('image-input');
+const imageInputButtonElement = document.getElementById('image-input-button');
+const imageInputTextElement = document.getElementById('image-input-text');
 const imageElement = document.getElementById('img');
 const asciiElement = document.getElementById('ascii');
 
@@ -37,3 +41,20 @@ let abortController = null;
 imageInputElement.addEventListener('change', (event) =>
 	handleImageInputChange(event, asciiElement, imageElement)
 );
+
+imageInputButtonElement.addEventListener('click', () =>
+	imageInputElement.click()
+);
+
+// Initialize image input with a gif
+{
+	const gifBlob = await fetch('./assets/triangle.gif');
+	const gif = new File([gifBlob], 'triangle.gif');
+	const changeEvent = new Event('change', {
+		target: {
+			files: [gif],
+		},
+	});
+
+	imageInputElement.dispatchEvent(changeEvent);
+}
