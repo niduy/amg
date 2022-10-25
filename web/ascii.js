e97a38893a08b7db79f854ec1fac929d3476adb1
niduy <37757504+niduy@users.noreply.github.com>
2022-10-25 10:56:23 +0400

    extract ascii stuff to a separate file

---
+++ web/ascii.js
@@ -0,0 +1,34 @@
+import init, { convert_img, convert_gif } from '../pkg/amg.js';
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const printAsciiImage = async (ascii_image, abortSignal, asciiElement) => {
+	if (abortSignal.aborted) return;
+
+	// Setting this amount of text in an element takes up to a few milliseconds, so we need to deduct this amount from the delay to keep the gif and the ascii gif in sync longer
+	const startDate = new Date().getTime();
+
+	asciiElement.textContent = ascii_image.ascii_string;
+	const finishDate = new Date().getTime();
+
+	const trueDelay = ascii_image.delay - (finishDate - startDate);
+
+	await sleep(trueDelay);
+};
+
+export const printAsciiImages = async (
+	ascii_images,
+	abortSignal,
+	asciiElement
+) => {
+	while (!abortSignal.aborted) {
+		for (const ascii_image of ascii_images) {
+			await printAsciiImage(ascii_image, abortSignal, asciiElement);
+		}
+	}
+};
+
+init();
+
+export const convertImg = convert_img;
+export const convertGif = convert_gif;
--- web/index.js
+++ web/index.js
@@ -1,30 +1,6 @@
-import init, { convert_img, convert_gif } from '../pkg/amg.js';
+import { printAsciiImages, convertImg, convertGif } from './ascii.js';
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const printAsciiImage = async (ascii_image, abortSignal) => {
-	if (abortSignal.aborted) return;
-
-	// Setting this amount of text in an element takes up to a few milliseconds, so we need to deduct this amount from the delay to keep the gif and the ascii gif in sync longer
-	const startDate = new Date().getTime();
-
-	asciiElement.textContent = ascii_image.ascii_string;
-	const finishDate = new Date().getTime();
-
-	const trueDelay = ascii_image.delay - (finishDate - startDate);
-
-	await sleep(trueDelay);
-};
-
-const printAsciiImages = async (ascii_images, abortSignal) => {
-	while (!abortSignal.aborted) {
-		for (const ascii_image of ascii_images) {
-			await printAsciiImage(ascii_image, abortSignal);
-		}
-	}
-};
-
-const handleImageInputChange = async (event) => {
+const handleImageInputChange = async (event, asciiElement, imageElement) => {
 	if (abortController) {
 		abortController.abort();
 		abortController = null;
@@ -34,24 +10,23 @@ const handleImageInputChange = async (event) => {
 	const isGif = file.type.split('/').pop() === 'gif';
 
 	const imageUrl = URL.createObjectURL(file);
+
 	const file_buffer = await file.arrayBuffer();
 	const uint8Array = new Uint8Array(file_buffer);
 
 	if (isGif) {
 		abortController = new AbortController();
-		const ascii_images = convert_gif(uint8Array);
+		const ascii_images = convertGif(uint8Array);
 
 		imageElement.src = imageUrl;
-		await printAsciiImages(ascii_images, abortController.signal);
+		await printAsciiImages(ascii_images, abortController.signal, asciiElement);
 		return;
 	}
 
-	const ascii_image = convert_img(uint8Array);
+	const ascii_image = convertImg(uint8Array);
 	imageElement.src = imageUrl;
 	asciiElement.textContent = ascii_image;
 };
-
-await init();
 
 const imageInputElement = document.getElementById('image-input');
 const imageElement = document.getElementById('img');
@@ -59,4 +34,6 @@ const asciiElement = document.getElementById('ascii');
 
 let abortController = null;
 
-imageInputElement.addEventListener('change', handleImageInputChange);
+imageInputElement.addEventListener('change', (event) =>
+	handleImageInputChange(event, asciiElement, imageElement)
+);
